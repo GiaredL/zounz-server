2f5447177a31da78ce7f9f3fcb0fe943cf1e23dd
Giared Mati <giared.mati@gmail.com>
2025-04-22 21:01:26 -0600

    more updates

--- src/server.js
+++ src/server.js
@@ -38,7 +38,12 @@ import {
               "http://localhost:3000",
               "http://localhost",
             ]
-          : "*", // Allow all origins in development
+          : [
+              "http://localhost:5173",
+              "http://localhost:5174",
+              "http://localhost:3000",
+              "http://localhost",
+            ],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
